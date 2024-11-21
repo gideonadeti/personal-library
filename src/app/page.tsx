a1840aef11e6c50ada8369240e26c6b35555022c
gideonadeti <gideonadeti0@gmail.com>
2024-11-21 17:17:31 +0000

    Redirect to all-books page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
-export default function Home() {
-  return <></>;
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Page() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/groups/all-books");
+  });
+
+  return null;
 }
