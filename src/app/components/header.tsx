4bcf039bd5b812992a335dadf2f0072bf5857c74
gideonadeti <gideonadeti0@gmail.com>
2024-11-21 18:33:07 +0000

    Set up Header component

---
+++ src/app/components/header.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import { UserButton } from "@clerk/nextjs";
+
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Separator } from "@/components/ui/separator";
+
+export default function Header() {
+  return (
+    <header className="flex items-center border-b py-2 ps-2 pe-4">
+      <SidebarTrigger />
+      <Separator orientation="vertical" className="mx-2" />
+      <Link href="/" className="text-lg font-semibold">
+        Personal Library
+      </Link>
+      <div className="ms-auto">
+        <UserButton showName />
+      </div>
+    </header>
+  );
+}
