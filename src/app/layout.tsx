ec3f0481026f625c431dc857cd637cd74f0813ae
gideonadeti <gideonadeti0@gmail.com>
2024-11-21 17:11:14 +0000

    Set up Clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
+import { ClerkProvider, SignedIn, SignedOut, SignIn } from "@clerk/nextjs";
 import localFont from "next/font/local";
 
 import "./globals.css";
+import { H2, H4 } from "./components/custom-tags";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,7 +31,20 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ClerkProvider>
+          <SignedIn>{children}</SignedIn>
+          <SignedOut>
+            <div className="flex flex-col md:flex-row items-center justify-around gap-8 h-screen overflow-y-auto p-8">
+              <div className="flex flex-col text-center lg:text-left">
+                <H2>Welcome to Personal Library</H2>
+                <H4>A web app that helps you manage you books</H4>
+              </div>
+              <div className="flex items-center justify-center">
+                <SignIn />
+              </div>
+            </div>
+          </SignedOut>
+        </ClerkProvider>
       </body>
     </html>
   );
