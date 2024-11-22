34c320bb223666654e57049a7bd0f3b5440fe805
gideonadeti <gideonadeti0@gmail.com>
2024-11-22 10:54:19 +0000

    Set up types for Group, Book, and Note

---
+++ src/app/types.ts
@@ -0,0 +1,22 @@
+export interface Note {
+  id: string;
+  bookId: string;
+  content: string;
+}
+
+export interface Book {
+  id: string;
+  groupId: string;
+  authorId: string;
+  genreId: string;
+  title: string;
+  status: "unread" | "reading" | "read";
+  favorite: boolean;
+  notes: Note[];
+}
+
+export interface Group {
+  id: string;
+  name: string;
+  books: Book[];
+}
