b17f7cf3e9245c437e49573113df9571445949ce
gideonadeti <gideonadeti0@gmail.com>
2024-11-29 07:18:34 +0000

    Rearrange imports

--- src/app/groups/[groupId]/layout.tsx
+++ src/app/groups/[groupId]/layout.tsx
@@ -1,6 +1,6 @@
+import Header from "@/app/components/header";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
-import Header from "@/app/components/header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
