dabde32570c7555a146c289bca1bc3b288b1da8a
gideonadeti <gideonadeti0@gmail.com>
2024-11-30 04:07:53 +0000

    Remove overflow hidden

--- src/app/(others)/layout.tsx
+++ src/app/(others)/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main className="flex-grow flex flex-col h-screen overflow-y-hidden">
+      <main className="flex-grow flex flex-col">
         <Header />
         {children}
       </main>
